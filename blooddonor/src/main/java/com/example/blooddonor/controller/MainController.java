0237c40b9efaa7ea26f093df4b05569c2761b1b2
bala <balajistudy5@gmail.com>
2024-06-07 17:33:28 +0530

    partially frontend completed

---
+++ blooddonor/src/main/java/com/example/blooddonor/controller/MainController.java
@@ -0,0 +1,32 @@
+package com.example.blooddonor.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class MainController {
+	
+	
+	
+	@RequestMapping("/login")
+	public String login() {
+		return "index";
+	}
+	
+	@RequestMapping("/register")
+	public String register() {
+		return "register";
+	}
+
+	@RequestMapping("/home")
+	public String home(@RequestParam(name = "fragmentToLoad") String fragmentToLoad, Model model, HttpSession session) {
+		session.setAttribute("role", "donor");
+		model.addAttribute("fragmentToLoad", fragmentToLoad);
+		return "home";
+	}
+}
